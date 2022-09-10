8f90d90b0c11731df1dc662bd40f2cf4ed655f2f
mrrevo55 <kwolny31@gmail.com>
2022-09-10 17:31:29 +0200

    Implement integration tests for service

--- src/test/java/com/lastcivilization/userwriteservice/utils/IntegrationBaseClass.java
+++ src/test/java/com/lastcivilization/userwriteservice/utils/IntegrationBaseClass.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -16,10 +17,7 @@ import java.util.HashMap;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(classes = UserWriteServiceApplication.class, webEnvironment = RANDOM_PORT, properties = {
-        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///test",
-        "spring.profiles.active=test"
-})
+@SpringBootTest(classes = UserWriteServiceApplication.class, webEnvironment = RANDOM_PORT, properties = "spring.profiles.active=test")
 @Testcontainers
 public class IntegrationBaseClass {
 
@@ -41,5 +39,6 @@ public class IntegrationBaseClass {
         registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
         registry.add("spring.rabbitmq.username", rabbitMQContainer::getAdminUsername);
         registry.add("spring.rabbitmq.password", rabbitMQContainer::getAdminPassword);
+        registry.add("spring.datasource.url", () -> "jdbc:tc:postgresql:14-alpine:///test");
     }
 }
