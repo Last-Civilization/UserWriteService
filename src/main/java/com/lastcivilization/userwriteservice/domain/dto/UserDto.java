ccda8f0001db8795be2ba9bc353387d14a640c39
mrrevo55 <kwolny31@gmail.com>
2022-09-13 21:19:18 +0200

    Reformatted code

--- src/main/java/com/lastcivilization/userwriteservice/domain/dto/UserDto.java
+++ src/main/java/com/lastcivilization/userwriteservice/domain/dto/UserDto.java
@@ -1,3 +1,11 @@
 package com.lastcivilization.userwriteservice.domain.dto;
 
-public record UserDto (Long id, String keycloakId, String login, String email, Long stats, Long equipment, int money){ }
+public record UserDto (
+        Long id,
+        String keycloakId,
+        String login,
+        String email,
+        Long stats,
+        Long equipment,
+        int money
+){ }
