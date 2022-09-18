8ce35b2c88bdb44d3b48492d5c4a77cd904842c3
mrrevo55 <kwolny31@gmail.com>
2022-09-18 22:02:55 +0200

    Update mapping annotation

--- src/main/java/com/lastcivilization/userwriteservice/infrastructure/service/equipment/EquipmentClient.java
+++ src/main/java/com/lastcivilization/userwriteservice/infrastructure/service/equipment/EquipmentClient.java
@@ -2,12 +2,13 @@ package com.lastcivilization.userwriteservice.infrastructure.service.equipment;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient("equipment-read-service")
 interface EquipmentClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/equipments")
+    @PostMapping("/equipments")
     Long createNewEquipment();
 }
