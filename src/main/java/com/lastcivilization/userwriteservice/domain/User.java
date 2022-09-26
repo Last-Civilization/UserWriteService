623447e754c5064b9305f642ed9ae72e190e7132
mrrevo55 <kwolny31@gmail.com>
2022-09-26 21:00:29 +0200

    Refactored code

--- src/main/java/com/lastcivilization/userwriteservice/domain/User.java
+++ src/main/java/com/lastcivilization/userwriteservice/domain/User.java
@@ -10,7 +10,7 @@ class User {
     private Long equipment;
     private Long account;
 
-    public User(Long id, String keycloakId, String login, String email, Long stats, Long equipment, Long account) {
+    User(Long id, String keycloakId, String login, String email, Long stats, Long equipment, Long account) {
         this.id = id;
         this.keycloakId = keycloakId;
         this.login = login;
@@ -20,63 +20,63 @@ class User {
         this.account = account;
     }
 
-    public Long getId() {
+    Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    void setId(Long id) {
         this.id = id;
     }
 
-    public String getKeycloakId() {
+    String getKeycloakId() {
         return keycloakId;
     }
 
-    public void setKeycloakId(String keycloakId) {
+    void setKeycloakId(String keycloakId) {
         this.keycloakId = keycloakId;
     }
 
-    public String getLogin() {
+    String getLogin() {
         return login;
     }
 
-    public void setLogin(String login) {
+    void setLogin(String login) {
         this.login = login;
     }
 
-    public String getEmail() {
+    String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
+    void setEmail(String email) {
         this.email = email;
     }
 
-    public Long getStats() {
+    Long getStats() {
         return stats;
     }
 
-    public void setStats(Long stats) {
+    void setStats(Long stats) {
         this.stats = stats;
     }
 
-    public Long getEquipment() {
+    Long getEquipment() {
         return equipment;
     }
 
-    public void setEquipment(Long equipment) {
+    void setEquipment(Long equipment) {
         this.equipment = equipment;
     }
 
-    public Long getAccount() {
+    Long getAccount() {
         return account;
     }
 
-    public void setAccount(Long account) {
+    void setAccount(Long account) {
         this.account = account;
     }
 
-    public static final class Builder {
+    static final class Builder {
 
         private Long id;
         private String keycloakId;
@@ -89,46 +89,46 @@ class User {
         private Builder() {
         }
 
-        public static Builder anUser() {
+        static Builder anUser() {
             return new Builder();
         }
 
-        public Builder id(Long id) {
+        Builder id(Long id) {
             this.id = id;
             return this;
         }
 
-        public Builder keycloakId(String keycloakId) {
+        Builder keycloakId(String keycloakId) {
             this.keycloakId = keycloakId;
             return this;
         }
 
-        public Builder login(String login) {
+        Builder login(String login) {
             this.login = login;
             return this;
         }
 
-        public Builder email(String email) {
+        Builder email(String email) {
             this.email = email;
             return this;
         }
 
-        public Builder stats(Long stats) {
+        Builder stats(Long stats) {
             this.stats = stats;
             return this;
         }
 
-        public Builder equipment(Long equipment) {
+        Builder equipment(Long equipment) {
             this.equipment = equipment;
             return this;
         }
 
-        public Builder account(Long account){
+        Builder account(Long account){
             this.account = account;
             return this;
         }
 
-        public User build() {
+        User build() {
             return new User(id, keycloakId, login, email, stats, equipment, account);
         }
     }
