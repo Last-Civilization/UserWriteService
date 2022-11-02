eeba5bc04d4929e26c862c2031fcf607fbe21b2b
mrrevo55 <kwolny31@gmail.com>
2022-11-02 21:08:17 +0100

    Refactored code

--- src/main/java/com/lastcivilization/userwriteservice/domain/Mapper.java
+++ src/main/java/com/lastcivilization/userwriteservice/domain/Mapper.java
@@ -2,7 +2,11 @@ package com.lastcivilization.userwriteservice.domain;
 
 import com.lastcivilization.userwriteservice.domain.view.UserModel;
 
-class Mapper {
+final class Mapper {
+
+    private Mapper(){
+        throw new RuntimeException("You can not create instance of this class!");
+    }
 
     static UserModel toDto(User user){
         return new UserModel(user.getId(), user.getKeycloakId(), user.getLogin(), user.getEmail(), user.getStats(), user.getEquipment(), user.getAccount());
